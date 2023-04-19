a5c5fd8b671db5db5b0456d731c30114151cc793
@whitep4nth3r <52798353+whitep4nth3r@users.noreply.github.com>
2023-04-19 14:48:28 +0100

    FIX TYPOOOO

--- plugins/mq.js
+++ plugins/mq.js
@@ -1,9 +1,9 @@
 import mq from "mqemitter";
 
 /** @param {import('fastify').FastifyInstance} fastify */
-export default async function mq(app, opts) {
+export default async function mqPlugin(app, opts) {
   const emitter = mq({ concurrency: 5 });
   app.decorate("mq", emitter);
 }
 
-mq[Symbol.for("skip-override")] = true;
+mqPlugin[Symbol.for("skip-override")] = true;
